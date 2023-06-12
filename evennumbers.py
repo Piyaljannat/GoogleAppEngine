9cfc4c17ac699b15f089d2f0865b085c6f61c287
Jannatun Naim Piyal <48407835+Piyaljannat@users.noreply.github.com>
2023-06-12 16:53:26 +0600

    Update evennumbers.py

--- evennumbers.py
+++ evennumbers.py
@@ -2,10 +2,15 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def generate_even_numbers(n):
-    numbers = [i for i in range(2, 2*n + 1, 2)]
-    return jsonify(numbers)
+@app.route('/generateEvenNumbers', methods=['GET', 'POST'])
+def generateEvenNumbers(n):
+    #numbers = [i for i in range(2, 2*n + 1, 2)]
+    #return jsonify(numbers)
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('result.html',form_data = form_data)
+ 
+    return render_template("form.html")
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
