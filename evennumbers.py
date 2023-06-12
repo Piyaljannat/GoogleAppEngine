45ca47bc181fb76e140b95ed9cb6a26ce6bd7772
Jannatun Naim Piyal <48407835+Piyaljannat@users.noreply.github.com>
2023-06-12 16:45:56 +0600

    Update evennumbers.py

--- evennumbers.py
+++ evennumbers.py
@@ -2,10 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/generate_even_numbers/<int:n>')
+@app.route('/')
 def generate_even_numbers(n):
     numbers = [i for i in range(2, 2*n + 1, 2)]
     return jsonify(numbers)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="127.0.0.1", port=8080, debug=True)
+    
+    
